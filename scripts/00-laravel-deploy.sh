a9bea51ab9527c0c431690a96f1ece7a7e97ea1a
devops <relabsso.sap@gmail.com>
2022-12-14 00:52:40 -0600

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer self-update
-#composer install --no-dev --working-dir=/var/www/html
+#composer self-update
+composer install --no-dev --working-dir=/var/www/html
 
-#echo "Caching config..."
-#php artisan config:cache
+echo "Caching config..."
+php artisan config:cache
 
-#echo "Caching routes..."
-#php artisan route:cache
+echo "Caching routes..."
+php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
